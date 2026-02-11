8ce0a145c6b7ff902739c3e4cb9be424d2859f3e
giuseppepetrazzuolo6 <giuseppepetrazzuolo6@gmail.com>
2026-02-11 11:08:32 +0100

    snack-5 bonus

--- main.js
+++ main.js
@@ -124,3 +124,21 @@ console.log(somma)
 //media età
 const etaMedia = somma / ages.length
 console.log(etaMedia)
+
+/*Snack 5 (Bonus) - Raccogli i libri
+Usando la l'API http://localhost:3333/books/{id} usa la combinazione di .map() e Promise.all(), 
+per creare una funzione (getBooks) che a partire da un array di id (ids), 
+ritorna una promise che risolve un array di libri (books).
+Testala con l’array [2, 13, 7, 21, 19] .*/
+
+async function getBooks(ids) {
+    return Promise.all(ids.map(id =>
+        fetch(`http://localhost:3333/books/${id}`).then(res => res.json())
+    ))
+}
+
+(async () => {
+    const ids = [2, 13, 7, 21, 19]
+    const books = await getBooks(ids)
+    console.log(books)
+})()
